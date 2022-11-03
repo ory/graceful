35123d6c1577bc8a95867b16813112af67e7f3a8
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-03 06:20:55 -0500

    chore: license headers

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
 // Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
 
 // Package graceful contains best practice http server configurations and helpers for Go 1.8's http graceful shutdown feature.
 package graceful
